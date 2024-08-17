8d9bbf47b6c3853310af3755b031ff5ea2275e0b
Vaibhav Zodge <zodgevaibhav@gmail.com>
2024-08-18 01:19:49 +0530

    learn(appCss): Create demo of using App.css for better UI

---
+++ 7.event-handle-use-state-login-screen-default-css/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleUserNameChange(event) {
+    setUserName(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function login() {
+    console.log(userName);
+    console.log(password);
+  }
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <h1>Login</h1>
+        <input
+          type="text"
+          onChange={handleUserNameChange}
+          placeholder="Enter user name"
+        />
+
+        <input
+          type="password"
+          onChange={handlePasswordChange}
+          placeholder="Enter user password"
+        />
+
+        <button onClick={login}>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
